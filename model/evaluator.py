6887d339d478ab979afb0074dc050552e139f1c2
Prajwal Gowda <gowdaprajwal418@gmail.com>
2026-03-26 20:19:56 +0530

    fix evaluator bug

--- model/evaluator.py
+++ model/evaluator.py
@@ -1,11 +1,21 @@
 from sentence_transformers import SentenceTransformer, util
 
-model = SentenceTransformer('all-MiniLM-L6-v2')
+model = None
+
+def get_model():
+    global model
+    if model is None:
+        model = SentenceTransformer('all-MiniLM-L6-v2')
+    return model
 
 def evaluate_answer(user_answer, expected_answer):
+    model = get_model()
+
+    # ✅ Create embeddings
     embeddings1 = model.encode(user_answer, convert_to_tensor=True)
     embeddings2 = model.encode(expected_answer, convert_to_tensor=True)
 
+    # ✅ Compute similarity
     similarity = util.pytorch_cos_sim(embeddings1, embeddings2)
 
     score = float(similarity[0][0]) * 100
